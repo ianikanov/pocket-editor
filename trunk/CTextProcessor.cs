efea167b38d88eaf06b1281371dfda06fa8d27d7
ivan.anikanov@gmail.com <ivan.anikanov@gmail.com@37673ac9-83d5-d54b-55af-e7b98d9fae76>
2010-09-20 13:24:13 +0000

    move logic into TextProcessor class. added classes for Languages

--- trunk/CTextProcessor.cs
+++ trunk/CTextProcessor.cs
@@ -8,6 +8,7 @@ namespace JSEditor
 {
     public class CTextProcessor
     {
+        #region Properties
         #region Control-specific properties
         /// <summary>
         /// Exact Text control
@@ -72,15 +73,211 @@ namespace JSEditor
         /// <summary>
         /// Gets is text in control was modified since open or last save
         /// </summary>
-        public bool IsModified { get; private set; }
+        public bool IsModified { get; set; }
+        /// <summary>
+        /// Language for current text
+        /// </summary>
+        public CLanguage Language { get; set; }
+        #endregion
+        #endregion
 
+        #region private fields
+        /// <summary>
+        /// Stored length of Text
+        /// </summary>
+        int textlen = 0;
+        #endregion
+
+        #region public methods
+        #region performing begin of line methods
+        /// <summary>
+        /// Insert specified pattern at the beginning of all selected lines
+        /// </summary>
+        /// <param name="newSpace">string to insert</param>
+        public void AddBeginning(string newSpace)
+        {
+            string[] ls = Lines;
+            string str = "";
+            int ret = 0;
+            for (int i = 0; i < LineCount; i++)
+            {
+#warning Not sure about \r
+                if (i >= SelectedLineIndex && i < SelectedLineIndex + SelectedLinesCount)
+                {
+                    str += ls[i].Insert((i == 0) ? 0 : 1, newSpace) + "\r";
+                    ret += newSpace.Length;
+                }
+                else
+                {
+                    str += ls[i] + "\r";
+                }
+            }
+            int[] val = new int[] { SelectedLineIndex, SelectedLinesCount };
+            Lines = ls;
+            SelectedLineIndex = val[0];
+            SelectedLinesCount = val[1];
+            ControlInstanse.ScrollToCaret();
+        }
+
+        /// <summary>
+        /// Remove specified pattern from begin of line
+        /// </summary>
+        /// <param name="newSpace">String to find</param>
+        /// <param name="allowSubStr">Allows to remove also any substring of pattern</param>
+        public void RemoveBeginning(string newSpace, bool allowSubStr)
+        {
+            string[] ls = Lines;
+            string str = "";
+            string line;
+            int cnt;
+            string pattern;
+            int ret = 0;
+            for (int i = 0; i < ls.Length; i++)
+            {
+                if (i >= SelectedLineIndex && i < SelectedLineIndex + SelectedLinesCount)
+                {
+                    cnt = newSpace.Length;
+                    pattern = newSpace;
+                    if (ls[i].StartsWith("\n"))
+                    {
+                        str += "\n";
+                        line = ls[i].Substring(1);
+                    }
+                    else line = ls[i];
+                    //ищем любые вхождения
+                    while (!line.StartsWith(pattern))
+                    {
+                        if (!allowSubStr)//если для любых запрещено - то выходим
+                        {
+                            cnt = 0;
+                            break;
+                        }
+                        if (--cnt == 0) break;
+                        pattern = newSpace.Substring(0, cnt);
+                    }
+                    //если нашли, то удаляем
+#warning Not sure about \r
+                    if (cnt > 0)
+                    {
+                        str += line.Substring(cnt) + "\r";
+                        ret += cnt;
+                    }
+                    else str += line + "\r";
+                }
+                else
+                {
+                    str += ls[i] + "\r";
+                }
+            }
+            int[] val = new int[] { SelectedLineIndex, SelectedLinesCount };
+            Lines = ls;
+            SelectedLineIndex = val[0];
+            SelectedLinesCount = val[1];
+            ControlInstanse.ScrollToCaret();
+        }
+        #endregion
+
+        #region Copy-Paste methods
+        /// <summary>
+        /// Return seleted text
+        /// </summary>
+        /// <returns></returns>
+        public string Copy()
+        {
+            return SelectedText;
+        }
+        /// <summary>
+        /// Remove selected text and return it
+        /// </summary>
+        /// <returns></returns>
+        public string Cut()
+        {
+            string txt = SelectedText;
+            int st = ControlInstanse.SelectionStart;
+            Text = Text.Substring(0, st) + Text.Substring(st + txt.Length);
+            ControlInstanse.SelectionStart = st;
+            ControlInstanse.ScrollToCaret();
+            return txt;
+        }
+        /// <summary>
+        /// Remove current selection and insert pattern
+        /// </summary>
+        /// <param name="ptrn"></param>
+        public void Paste(string ptrn)
+        {
+            int curPosition = ControlInstanse.SelectionStart;
+            Text = Text.Remove(curPosition, ControlInstanse.SelectionLength).Insert(curPosition, ptrn);
+            ControlInstanse.Select(curPosition, ptrn.Length);
+            ControlInstanse.ScrollToCaret();
+        }
+        #endregion
+
+        #region Text changed
+        /// <summary>
+        /// Make whole logic according to changes in text
+        /// </summary>
+        public void TextChanged()
+        {
+            int curPosition;
+            IsModified = true;
+            if (Text.Length > textlen && ControlInstanse.SelectionStart > 0)//added and not inserted
+            {
+                if (Text[ControlInstanse.SelectionStart - 1] == '\n')
+                {
+                    try
+                    {
+                        int spacesCount = 0;
+                        string newSpace = "";
+                        if (SelectedLineIndex != 1) spacesCount++;
+                        while ((Lines[SelectedLineIndex - 1].Length > spacesCount) && (Lines[SelectedLineIndex - 1][spacesCount++] == ' ')) newSpace += " ";
+                        AddBeginning(newSpace);
+                        //curPosition = ControlInstanse.SelectionStart;
+                        //Text = Text.Insert(curPosition, newSpace);
+                        //txtMain.SelectionStart = curPosition + newSpace.Length;
+                        //txtMain.ScrollToCaret();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                if (Text[ControlInstanse.SelectionStart - 1] == '\t')
+                {
+                    curPosition = ControlInstanse.SelectionStart;
+                    Text = Text.Replace("\t", Settings.TabSwap);
+                    ControlInstanse.SelectionStart = curPosition + 1;
+                    ControlInstanse.ScrollToCaret();
+                }
+            }
+            textlen = Text.Length;
+        }
+        #endregion
+        #endregion
+
+        #region Private methods
+        /// <summary>
+        /// Get word between separators, that contains specified index (or if null - Selection start)
+        /// </summary>
+        /// <param name="txtIndex"></param>
+        /// <returns></returns>
+        private Word GetWord(int? txtIndex)
+        {
+            Word wr = new Word();
+            int i = txtIndex.HasValue ? txtIndex.Value : ControlInstanse.SelectionStart;
+            while (!Language.Separators.Contains(Text[i].ToString()) && --i > 0) ;
+            wr.startPosition = i++;
+            while (!Language.Separators.Contains(Text[i].ToString()) && ++i > 0) ;
+            wr.length = i - wr.startPosition;
+            wr.text = Text.Substring(wr.startPosition, wr.length);
+            return wr;
+        }
         #endregion
     }
 
-    public enum DevelopLanguages
-    { 
-        Php,
-        ActionScript,
-        CSharp
+    public struct Word
+    {
+        public string text;
+        public int startPosition;
+        public int length;
     }
 }
--- trunk/cRichTextBox.cs
+++ trunk/cRichTextBox.cs
@@ -10,268 +10,101 @@ namespace JSEditor
 {
     public partial class cRichTextBox : UserControl
     {
-        int textlen;
+        public CTextProcessor TextProcessor { get; set; }
+
         public cRichTextBox()
         {
             InitializeComponent();
-            textlen = 0;
+            _tab = new TabPage();
+            this.Dock = DockStyle.Fill;
+            _tab.Controls.Add(this);
             TextProcessor = new CTextProcessor();
             TextProcessor.ControlInstanse = txtMain;
         }
 
-        public CTextProcessor TextProcessor { get; set; }
+        #region properties
+        private TabPage _tab;
+        public TabPage Tab
+        {
+            get 
+            {
+                return _tab;
+            }
+        }
 
         public string FileName { get; set; }
-        public bool IsChanged { get; set; }
-        public override string Text { get { return txtMain.Text; } set { txtMain.Text = value; } }
-        public string SelectedText { get { return txtMain.SelectedText; } }
-        public int Language { get; set; }
+        public string TabName { get { return _tab.Text; } set { _tab.Text = value; } }
+        #endregion
 
-
+        #region events
         private void txtMain_TextChanged(object sender, EventArgs e)
         {
-            int curPosition;
-            IsChanged = true;
-            if (txtMain.Text.Length > textlen && txtMain.SelectionStart > 0)//added and not inserted
-            {
-                if (txtMain.Text[txtMain.SelectionStart - 1] == '\n')
-                {
-                    recalcLines();
-                    try
-                    {
-                        int spacesCount = 0;
-                        int linePos = GetLineNumber();
-                        string[] lines = GetLines();
-                        string newSpace = "";
-                        if (linePos != 1) spacesCount++;
-                        while ((lines[linePos - 1].Length > spacesCount) && (lines[linePos - 1][spacesCount++] == ' ')) newSpace += " ";
-                        curPosition = txtMain.SelectionStart;
-                        txtMain.Text = txtMain.Text.Insert(curPosition, newSpace);
-                        txtMain.SelectionStart = curPosition + newSpace.Length;
-                        txtMain.ScrollToCaret();
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message);
-                    }
-                }
-                if (txtMain.Text[txtMain.SelectionStart - 1] == '\t')
-                { 
-                    curPosition = txtMain.SelectionStart;
-                    txtMain.Text = txtMain.Text.Replace("\t", "  ");
-                    txtMain.SelectionStart = curPosition + 1;
-                    txtMain.ScrollToCaret();
-                }
-            }
-            textlen = txtMain.Text.Length;
+            TextProcessor.TextChanged();
         }
 
-        #region tech
-        private void recalcLines()
-        {
-            int max = GetLines().Length;
-            string ss = "";
-            for (int i = 0; i < max; i++) ss += i + "\r\n";
-            lblLines.Text = ss;
-        }
-
-        /// <summary>
-        /// Получить начальный номер строки
-        /// </summary>
-        /// <returns></returns>
-        private int GetLineNumber()
-        {
-            return GetLineNumber(false);
-        }
-
-        /// <summary>
-        /// Получить номер строки выделения
-        /// </summary>
-        /// <param name="toEnd">false - первая строка, true - последняя строка</param>
-        /// <returns></returns>
-        private int GetLineNumber(bool toEnd)
-        {
-            return txtMain.Text.Substring(0, toEnd ? (txtMain.SelectionLength + txtMain.SelectionStart) : txtMain.SelectionStart).Split((char)Keys.Enter).Length - 1;
-        }
-
-        /// <summary>
-        /// Получить текст, разбитый на строки
-        /// </summary>
-        /// <returns></returns>
-        private string[] GetLines()
-        {
-            string txt = txtMain.Text;
-            return txt.Split((char)Keys.Enter);
-        }
-
-        /// <summary>
-        /// Вставить строчку в начало всех выделенных строк
-        /// </summary>
-        /// <param name="newSpace">Строка для вставки</param>
-        private void InsertToLineStart(string newSpace)
-        {
-            int lst = GetLineNumber();
-            int lfin = GetLineNumber(true);
-            string[] ls = GetLines();
-            string str = "";
-            int ret = 0;
-            for (int i = 0; i < ls.Length; i++)
-            {
-                if (i >= lst && i <= lfin)
-                {
-                    str += ls[i].Insert((i == 0) ? 0 : 1, newSpace) + "\r";
-                    ret += newSpace.Length;
-                }
-                else
-                {
-                    str += ls[i] + "\r";
-                }
-            }
-            int curPosition = txtMain.SelectionStart;
-            int len = txtMain.SelectionLength;
-            txtMain.Text = str.Substring(0, str.Length - 1);
-            txtMain.SelectionStart = curPosition;
-            if (len > 0) txtMain.SelectionLength = len + ret;
-            txtMain.ScrollToCaret();
-        }
-
-        /// <summary>
-        /// Удалить строчку из начала всех выделенных строк
-        /// </summary>
-        /// <param name="newSpace">Строка для удаления</param>
-        /// <param name="allowSubStr">Удалять ли так же любую подстроку данной</param>
-        private void RemoveFromLineStart(string newSpace, bool allowSubStr)
-        {
-            int lst = GetLineNumber();
-            int lfin = GetLineNumber(true);
-            string[] ls = GetLines();
-            string str = "";
-            string line;
-            int cnt;
-            string pattern;
-            int ret = 0;
-            for (int i = 0; i < ls.Length; i++)
-            {
-                if (i >= lst && i <= lfin)
-                {
-                    cnt = newSpace.Length;
-                    pattern = newSpace;
-                    if (ls[i].StartsWith("\n"))
-                    {
-                        str += "\n";
-                        line = ls[i].Substring(1);
-                    }
-                    else line = ls[i];
-                    //ищем любые вхождения
-                    while (!line.StartsWith(pattern))
-                    {
-                        if (!allowSubStr)//если для любых запрещено - то выходим
-                        {
-                            cnt = 0;
-                            break;
-                        }
-                        if (--cnt == 0) break;
-                        pattern = newSpace.Substring(0, cnt);
-                    }
-                    //если нашли, то удаляем
-                    if (cnt > 0)
-                    {
-                        str += line.Substring(cnt) + "\r";
-                        ret += cnt;
-                    }
-                    else str += line + "\r";
-                }
-                else
-                {
-                    str += ls[i] + "\r";
-                }
-
-            }
-            int curPosition = txtMain.SelectionStart;
-            int len = txtMain.SelectionLength;
-            txtMain.Text = str.Substring(0, str.Length - 1);
-            txtMain.SelectionStart = curPosition;
-            if (len > 0) txtMain.SelectionLength = len - ret;
-            txtMain.ScrollToCaret();
-        }
-
-        /// <summary>
-        /// Удалить строчку из начала всех выделенных строк
-        /// </summary>
-        /// <param name="newSpace">Строка для удаления</param>
-        private void RemoveFromLineStart(string newSpace)
-        {
-            RemoveFromLineStart(newSpace, false);
-        }
-
-        internal void FocusText()
+        public void FocusText()
         {
             txtMain.Focus();
-        }
-
-        internal void SelectText(int pos, int len)
-        {
-            txtMain.SelectionStart = pos;
-            txtMain.SelectionLength = len;
-            txtMain.ScrollToCaret();
         }
         #endregion
 
         #region ContextMenu
         private void miCut_Click(object sender, EventArgs e)
         {
-            Clipboard.SetDataObject(txtMain.SelectedText);
-            int cursor = txtMain.SelectionStart;
-            txtMain.Text = txtMain.Text.Substring(0, cursor) + txtMain.Text.Substring(cursor + txtMain.SelectionLength, txtMain.TextLength - (cursor + txtMain.SelectionLength));
-            txtMain.SelectionStart = cursor;
-            txtMain.ScrollToCaret();
+            Clipboard.SetDataObject(TextProcessor.Cut());
         }
 
         private void miCopy_Click(object sender, EventArgs e)
         {
-            Clipboard.SetDataObject(txtMain.SelectedText);
+            Clipboard.SetDataObject(TextProcessor.Copy());
         }
 
         private void miPaste_Click(object sender, EventArgs e)
         {
-            int curPosition = txtMain.SelectionStart;
-            string toPaste;
             try
             {
-                toPaste = Clipboard.GetDataObject().GetData(typeof(string)).ToString();
+                TextProcessor.Paste(Clipboard.GetDataObject().GetData(typeof(string)).ToString());
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
                 return;
             }
-            txtMain.Text = txtMain.Text.Remove(txtMain.SelectionStart, txtMain.SelectionLength).Insert(curPosition, toPaste);
-            txtMain.SelectionStart = curPosition + toPaste.Length;
-            txtMain.ScrollToCaret();
+            
         }
         #endregion
 
-        #region Tab
+        #region public control methods
         public void TabRight()
         {
-            InsertToLineStart(Settings.TabSwap);
+            TextProcessor.AddBeginning(Settings.TabSwap);
         }
 
         public void TabLeft()
         {
-            RemoveFromLineStart(Settings.TabSwap, true);
+            TextProcessor.RemoveBeginning(Settings.TabSwap, true);
         }
-        #endregion
 
-        #region comment
         public void Comment()
         {
-            InsertToLineStart("//");
+            TextProcessor.AddBeginning("//");
         }
 
         public void Uncomment()
         {
-            RemoveFromLineStart("//");
+            TextProcessor.RemoveBeginning("//", false);
+        }
+        #endregion
+
+        #region File
+        public bool? Save()
+        {
+            return null;
+        }
+
+        public bool Load()
+        {
+            return false;
         }
         #endregion
     }
