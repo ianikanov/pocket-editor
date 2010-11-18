bd89e89123370911d4b3754630208271995af351
ivan.anikanov@gmail.com <ivan.anikanov@gmail.com@37673ac9-83d5-d54b-55af-e7b98d9fae76>
2010-11-18 13:40:15 +0000

    add launcher - this form should perform exception handling and restart of application add CFileManager - this class provide work with dialogs and save/load files fixes in text processor

---
+++ CFileManager.cs
@@ -0,0 +1,249 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace JSEditor
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CFileManager
+    {
+        #region Private fields
+        private string _fileExtensionsList = "PHP files|*.php|ActionScript files|*.as|HTM files|*.htm|HTML files|*.html|JavaScript files|*.js|C# files|*.cs|C++ files|*.cpp|SQL files|*.sql|All code files|*.php;*.as;*.htm;*.html;*.js;*.cs;*.cpp;*.sql|All files|*.*";
+        private List<FileInfo> _OpenedFiles = new List<FileInfo>();
+        #endregion
+
+        #region Open file
+        /// <summary>
+        /// Open and read specified file and keep link
+        /// </summary>
+        /// <param name="path">file path to open</param>
+        /// <param name="text">return file content</param>
+        /// <returns>JSFile struct, that can be used in other methods</returns>
+        public JSFile OpenFile(string path, out string text)
+        {
+            return OpenFile(path, true, out text);
+        }
+
+        /// <summary>
+        /// Open and read specified file
+        /// </summary>
+        /// <param name="path">file path to open</param>
+        /// <param name="keepInMemory">set is link to this file should be stored in memory</param>
+        /// <param name="text">return file content</param>
+        /// <returns>JSFile struct, that can be used in other methods</returns>
+        public JSFile OpenFile(string path, bool keepInMemory, out string text)
+        {
+            JSFile fd = new JSFile();
+            fd.descriptor = -1;
+            FileInfo fil = new FileInfo(path);
+            if (!fil.Exists)
+            {
+                throw new FileNotFoundException(string.Format("File {0} not found.", path));
+            }
+            text = null;
+            StreamReader sr = null;
+            try
+            {
+                sr = fil.OpenText();
+                text = sr.ReadToEnd().Replace("\t", Settings.TabSwap);
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                if (sr != null) sr.Close();
+                throw new EndOfStreamException("Can not read file " + path, ex);
+            }
+            if (keepInMemory)
+            {
+                _OpenedFiles.Add(fil);
+                fd.descriptor = _OpenedFiles.Count - 1;
+            }
+            return fd;
+        }
+        #endregion
+
+        #region Save file
+        /// <summary>
+        /// Save previously opened file with new text
+        /// </summary>
+        /// <param name="fd">Internal file descriptor</param>
+        /// <param name="text">Text to store</param>
+        public void SaveFile(JSFile fd, string text)
+        {
+            FileInfo fil = _OpenedFiles[fd.descriptor];
+            SaveFile(fil, text);
+        }
+
+        /// <summary>
+        /// Save file into specified path and store link in memory
+        /// </summary>
+        /// <param name="path">Phisical path of file</param>
+        /// <param name="text">Text to store</param>
+        /// <returns>Internal file descriptor</returns>
+        public JSFile SaveFile(string path, string text)
+        {
+            return SaveFile(path, text, true);
+        }
+
+        /// <summary>
+        /// Save file into specified path
+        /// </summary>
+        /// <param name="path">Phisical path of file</param>
+        /// <param name="text">Text to store</param>
+        /// <param name="keepInMemory">Specify need to store link in memory or not</param>
+        /// <returns>Internal file descriptor</returns>
+        public JSFile SaveFile(string path, string text, bool keepInMemory)
+        {
+            FileInfo fil = new FileInfo(path);
+            SaveFile(fil, text);
+            JSFile fd = new JSFile();
+            fd.descriptor = -1;
+            if (keepInMemory)
+            {
+                _OpenedFiles.Add(fil);
+                fd.descriptor = _OpenedFiles.Count - 1;
+            }
+            return fd;
+        }
+
+        /// <summary>
+        /// Make saving text into specified file
+        /// </summary>
+        /// <param name="fil">Info about target file</param>
+        /// <param name="text">Text to store</param>
+        private void SaveFile(FileInfo fil, string text)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = fil.CreateText();
+                sw.Write(text.Replace(Settings.TabSwap, "\t"));
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                if (sw != null) sw.Close();
+                throw new EndOfStreamException("File can not be written: " + fil.Name, ex);
+            }
+        }
+        #endregion
+
+        #region Remove file
+        /// <summary>
+        /// Removed link to specified file from memeory
+        /// </summary>
+        /// <param name="fd">File descriptor</param>
+        public void CloseFile(JSFile fd)
+        {
+            _OpenedFiles.RemoveAt(fd.descriptor);
+        }
+        #endregion
+
+        #region FileInfo
+        /// <summary>
+        /// Returns extension of stored file
+        /// </summary>
+        /// <param name="fd">File descriptor</param>
+        /// <returns>Extention</returns>
+        public string GetExtension(JSFile fd)
+        {
+            return _OpenedFiles[fd.descriptor].Extension;
+        }
+
+        /// <summary>
+        /// Returns full name with path of stored file
+        /// </summary>
+        /// <param name="fd">File descriptor</param>
+        /// <returns>Extention</returns>
+        public string GetFullName(JSFile fd)
+        {
+            return _OpenedFiles[fd.descriptor].FullName;
+        }
+
+        /// <summary>
+        /// Returns name of stored file
+        /// </summary>
+        /// <param name="fd">File descriptor</param>
+        /// <returns>Extention</returns>
+        public string GetName(JSFile fd)
+        {
+            return _OpenedFiles[fd.descriptor].Name;
+        }
+        #endregion
+
+        #region Dialogs
+        /// <summary>
+        /// Displays OpenFile dialog and return file path or null if cancel
+        /// </summary>
+        /// <param name="selected">default extension filter is set by selected language</param>
+        /// <returns>File path</returns>
+        public string ShowOpenFileDialog(DevelopLanguages selected)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = _fileExtensionsList;
+            switch (selected)
+            {
+                case DevelopLanguages.ActionScript: ofd.FilterIndex = 2; break;
+                case DevelopLanguages.Cpp: ofd.FilterIndex = 7; break;
+                case DevelopLanguages.CSharp: ofd.FilterIndex = 6; break;
+                case DevelopLanguages.Html: ofd.FilterIndex = 3; break;
+                case DevelopLanguages.Javascript: ofd.FilterIndex = 5; break;
+                case DevelopLanguages.Php: ofd.FilterIndex = 1; break;
+                case DevelopLanguages.Sql: ofd.FilterIndex = 8; break;
+                default: ofd.FilterIndex = 1; break;
+            }
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                return ofd.FileName;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Displays SaveFile dialog and return file path or null if cancel
+        /// </summary>
+        /// <param name="selected">default extension filter is set by selected language</param>
+        /// <returns>File path</returns>
+        public string ShowSaveFileDialog(DevelopLanguages selected)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = _fileExtensionsList;
+            switch (selected)
+            {
+                case DevelopLanguages.ActionScript: sfd.FilterIndex = 2; break;
+                case DevelopLanguages.Cpp: sfd.FilterIndex = 7; break;
+                case DevelopLanguages.CSharp: sfd.FilterIndex = 6; break;
+                case DevelopLanguages.Html: sfd.FilterIndex = 3; break;
+                case DevelopLanguages.Javascript: sfd.FilterIndex = 5; break;
+                case DevelopLanguages.Php: sfd.FilterIndex = 1; break;
+                case DevelopLanguages.Sql: sfd.FilterIndex = 8; break;
+                default: sfd.FilterIndex = 1; break;
+            }
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                return sfd.FileName;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// Structure, used for represent file descriptor for JSEditor
+    /// </summary>
+    public struct JSFile
+    {
+        public int descriptor;
+    }
+}
---
+++ ErrorForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace JSEditor
+{
+    partial class ErrorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.txtDetails = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Location = new System.Drawing.Point(4, 4);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(349, 20);
+            this.lblMessage.Text = "label1";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.txtDetails);
+            this.panel1.Controls.Add(this.vScrollBar1);
+            this.panel1.Location = new System.Drawing.Point(4, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(349, 146);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.vScrollBar1.Location = new System.Drawing.Point(336, 0);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(13, 146);
+            this.vScrollBar1.TabIndex = 0;
+            // 
+            // btnNo
+            // 
+            this.btnNo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNo.Location = new System.Drawing.Point(302, 178);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(47, 20);
+            this.btnNo.TabIndex = 2;
+            this.btnNo.Text = "Abort";
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // btnYes
+            // 
+            this.btnYes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnYes.Location = new System.Drawing.Point(173, 178);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(61, 20);
+            this.btnYes.TabIndex = 4;
+            this.btnYes.Text = "Continue";
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRestart.Location = new System.Drawing.Point(240, 178);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(56, 20);
+            this.btnRestart.TabIndex = 8;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // txtDetails
+            // 
+            this.txtDetails.AcceptsReturn = true;
+            this.txtDetails.AcceptsTab = true;
+            this.txtDetails.Dock = System.Windows.Forms.DockStyle.Left;
+            this.txtDetails.Location = new System.Drawing.Point(0, 0);
+            this.txtDetails.Multiline = true;
+            this.txtDetails.Name = "txtDetails";
+            this.txtDetails.ReadOnly = true;
+            this.txtDetails.Size = new System.Drawing.Size(325, 146);
+            this.txtDetails.TabIndex = 1;
+            // 
+            // ErrorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(356, 203);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblMessage);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ErrorForm";
+            this.Text = "Critical Error";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.TextBox txtDetails;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
